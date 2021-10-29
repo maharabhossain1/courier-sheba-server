a83277a661bc96dba9658265e9640a3f2a4812ee
maharabhossain1 <maharabhossain0022@gmail.com>
2021-10-29 06:58:26 +0600

    firsttime

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+////////////////////////
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+//////////////////////////
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhwso.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("services");
+    const servicesCollections = database.collection("service");
+
+    console.log("conneted");
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+/////////////////////////////
+app.get("/", (req, res) => {
+  res.send("runnign my code1");
+});
+
+app.listen(port, () => {
+  console.log("runnig", port);
+});
